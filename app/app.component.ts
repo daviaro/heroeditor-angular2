7235845cd069a022455f5785ebaaf0f3d6ec141e
Adalbert David Aroca <daviaro@gmail.com>
2016-10-06 01:49:22 -0500

    Se realiza cambios en los fuentes, se agregan cambios en el selector del app.component.ts, se se escribio la clase hero, se cambio el export class AppComponente usando en el arreglo heroes el objeto heroe

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,47 @@
 import { Component } from '@angular/core';
+import {Hero} from "./hero";
 
 @Component({
-    selector: 'my-app',
-    template: '<h1>My First Angular App</h1>'
+    selector: 'hero-editor',
+    /* Cambiando forma de presentar el template */
+    /*template: `<h1>{{title}}</h1>
+               <h2>My favorite hero is: {{myHero}}</h2>
+               <p>Heroes:</p>
+                <ul>
+                  <li *ngFor="let hero of heroes">
+                  {{ hero }}
+                  </li>
+                </ul>
+`*/
+    /*Con objetos*/
+    template: `
+                <h1>{{title}}</h1>
+                <h2>My favorite hero is: {{myHero.name}}</h2>
+                <p>Heroes:</p>
+                <ul>
+                  <li *ngFor="let hero of heroes">
+                   {{ hero.id }} -{{ hero.name }} 
+                  </li>
+                </ul>
+    `
+
 })
-export class AppComponent { }
+/*
+  Usando texto normal
+  export class AppComponent {
+  title = 'Tour of Heroes';
+  heroes = ['Windstorm', 'Bombasto', 'Magneta', 'Tornado'];
+  myHero = this.heroes[0];
+}*/
+
+export class AppComponent {
+  title = 'Tour of Heroes';
+  heroes = [
+    new Hero(1, 'Windstorm'),
+    new Hero(13, 'Bombasto'),
+    new Hero(15, 'Magneta'),
+    new Hero(20, 'Tornado')
+  ];
+  myHero = this.heroes[0];
+}
+
